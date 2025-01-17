5b5a81d2289f175def25ffa154805e60838674dd
Alice <0210587164a@gmail.com>
2025-01-17 12:05:40 +0900

    feat: update app

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 st.title('Deploy APP')
-
 st.write('My First Deploy APP !')
 
+import os
+
+key= os.environ.get('MY_SECRET','NOT SET YET')
+st.write(f'Server Key: {key}')
+
+#git pull origin master
+#Merge ----
+#:q
+
+
+#git push origin master
